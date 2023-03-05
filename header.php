55a6c84dd0b4c6496f99fe6a437ff05463011de9
Ivan Borkov <i.borkoff@yandex.ru>
2023-03-05 13:45:32 +0300

    Test FirstPage

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="header">
+        <div class="container">
+            <div class="header-line">
+                <div class="logo">
+                    <a href="#"><img class="logo-img" src="free-icon-blog-9398467.png" alt=""></a>
+                </div>
+                <div class="nav">
+                <a class="nav-el" href="#">Зарегистрироваться</a>    
+                <a class="nav-el" href="#">Войти</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
--- index.php
+++ index.php
@@ -1,12 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>fpdsafpdspf</h1>
-</body>
-</html>
+<?php
+    include_once 'header.php';
+?>
+
+<div class="content">
+    <div class="container">
+        <h1>Присоединяйся к нам!</h1>
+        <div class="row">
+            <div class="cols img">
+                <img src="image_1_u11.jpg" alt="">
+            </div>
+            <div class="cols text">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, nisi doloribus accusamus harum, quam veritatis sunt facere et odio sint eveniet adipisci excepturi, magni laborum illum vel debitis ab vitae commodi omnis asperiores modi architecto assumenda nihil. Eos voluptatibus maxime minus quis fugit sunt sed similique perspiciatis nihil consequuntur numquam eaque corporis tenetur exercitationem libero, dolorem esse quibusdam, doloribus sit. Recusandae porro culpa officia beatae incidunt laborum voluptatibus soluta reprehenderit, veritatis, rerum debitis eveniet quisquam alias doloribus nulla? Corrupti illum, soluta consequatur atque error doloremque magnam sint alias consectetur sapiente, maxime hic voluptatibus autem. Eos, aperiam suscipit! Earum, quam nesciunt.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+    include_once 'footer.php';
+?>
