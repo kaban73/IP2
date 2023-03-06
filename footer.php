41e825ab79bece7dc8fcfc46fab73635214c1825
Ivan Borkov <i.borkoff@yandex.ru>
2023-03-06 16:50:33 +0300

    First Page on flex is ok

--- footer.php
+++ footer.php
@@ -1,2 +1,14 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row-f">
+            <div class="desc">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, libero? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, libero?</p>
+            </div>
+            <div class="about_us">
+                <a href="#">О нас</a>
+            </div>
+        </div>
+    </div>
+</footer>
 </body>
 </html>
--- index.php
+++ index.php
@@ -3,7 +3,19 @@
         include_once 'header.php';
     ?>
 
-
+    <div class="content">
+        <div class="container">
+        <h1>Присоединяйся к нам!</h1>
+            <div class="row">
+                <div class="pic">
+                    <img src="image_1_u11.jpg" alt="">
+                </div>
+                <div class="text">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit animi impedit laboriosam iusto doloribus voluptas, nemo at sed quo eaque nobis rem officia adipisci, temporibus quas perspiciatis obcaecati. Libero cupiditate ipsam et dolorum, unde dicta minus officia dignissimos minima soluta nihil, sit repudiandae incidunt molestias cumque reiciendis vitae itaque vero impedit nam porro. Facilis minima quaerat dolor consequatur consequuntur ab suscipit deleniti dignissimos recusandae quas. In deserunt quam ex ea beatae molestiae ad cumque et adipisci repudiandae molestias saepe excepturi fuga necessitatibus officia aperiam dolores doloribus, minima cum qui iusto? Repellendus distinctio fugit, at ex vero placeat sit ea magnam.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <?php
         include_once 'footer.php';
