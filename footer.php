01963991babab40eedb36984b0c0689b135e864b
Ivan Borkov <i.borkoff@yandex.ru>
2023-03-06 08:38:43 +0300

    Test FirstPage

--- footer.php
+++ footer.php
@@ -1,2 +1,15 @@
+<footer class="footer">
+    <div class="container">
+        <div class="footer-con">
+            <div class="descr">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus, ipsa? Est atque, repellendus quibusdam explicabo vitae facere dignissimos unde nam laborum eos, facilis suscipit voluptate consequuntur ipsam tempore laboriosam quidem!
+            </div>
+            <div class="about_us">
+                <a href="#">О нас</a>
+            </div>
+        </div>
+    </div>
+</footer>
+</div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<div class="wrapper">
 <?php
     include_once 'header.php';
 ?>
