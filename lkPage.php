6aaa1b0dd29f9cd95c1712cde19623e41b94463f
Ivan Borkov <i.borkoff@yandex.ru>
2023-03-09 17:03:08 +0300

    lkPage is ready

--- lkPage.php
+++ lkPage.php
@@ -14,43 +14,66 @@
             <div class="row">
                 <div class="nav-panel">
                     <table class="nav-table">
-                        <tr><td onclick="window.location.href='/lkPage.php'; return false">adsdas</td></tr>
-                        <tr><td onclick="window.location.href='/lkPage.php'; return false">adsdas</td></tr>
-                        <tr><td onclick="window.location.href='/lkPage.php'; return false">adsdas</td></tr>
-                        <tr><td onclick="window.location.href='/lkPage.php'; return false">adsdas</td></tr>
-                        <tr><td onclick="window.location.href='/lkPage.php'; return false">adsdas</td></tr>
+                        <tr><td onclick="window.location.href='/lkPage.php'; return false">Личный кабинет</td></tr>
+                        <tr><td onclick="window.location.href='/lkPage.php'; return false">Мои друзья</td></tr>
+                        <tr><td onclick="window.location.href='/lkPage.php'; return false">Истории из жизни</td></tr>
+                        <tr><td onclick="window.location.href='/lkPage.php'; return false">Мой путь</td></tr>
+                        <tr><td onclick="window.location.href='/lkPage.php'; return false">Любимые фото</td></tr>
                     </table>
                 </div>
                 <div class="content-panel">
                     <div class="title">
-                        Lkkkkkkk
+                        Приветствуем Вас в вашем личном кабинете!
                     </div>
                     <div class="row content-content">
                         <div class="avatar">
-                            <img src="images/free-icon-blog-9398467.png" alt="">
+                            <img src="images/avatar.jpg" width="256px" alt="">
                             <a href="#">изменить фото</a>
                         </div>
                         <div class="data">
                             <div class="data-el">
-                                Ваш псевдоним: <br>
-                                <b>Ivan</b>
+                                <div class="el-title">
+                                    Ваше имя:
+                                </div>
+                                <div class="el-text">
+                                    Иван
+                                </div>
                             </div>
                             <div class="data-el">
-                                Ваш псевдоним: <br>
-                                <b>Ivan</b>
+                                <div class="el-title">
+                                    Ваша фамилия:
+                                 </div>
+                                <div class="el-text">
+                                    Борков
+                                </div>
                             </div>
                             <div class="data-el">
-                                Ваш псевдоним: <br>
-                                <b>Ivan</b>
+                                <div class="el-title">
+                                    Ваш псевдоним:
+                                </div>
+                                <div class="el-text">
+                                    kaban73
+                                </div>
                             </div>
                             <div class="data-el">
-                                Ваш псевдоним: <br>
-                                <b>Ivan</b>
+                                <div class="el-title">
+                                    Ваш пол:
+                                </div>
+                                <div class="el-text">
+                                    мужской
+                                </div>
+                            </div>
+                            <div class="data-el">
+                                <a href="#">Изменить данные</a>
                             </div>
                         </div>
                         <div class="information">
-                            <b>Info:</b> <br>
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos eum aliquid neque placeat alias! Quisquam culpa doloribus, atque laudantium placeat totam molestiae, voluptates quibusdam quasi corporis odio vel nostrum optio
+                            <div class="el-title">
+                                Кратко о себе:
+                            </div>
+                            <div class="el-info">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia libero possimus expedita nemo, velit dicta autem illum animi perferendis neque ab quasi quae laboriosam in et consequuntur deserunt quibusdam eos?
+                            </div>
                         </div>
                     </div>
                 </div>
