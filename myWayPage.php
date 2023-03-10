494bd55b02781b46ae295b8b3b3fa649a2ee4478
Ivan Borkov <i.borkoff@yandex.ru>
2023-03-10 13:00:01 +0300

    myWayPage wh text is ready

---
+++ myWayPage.php
@@ -0,0 +1,36 @@
+<div class="wrapper">
+
+    <?php
+        $url = "styles/style_myWayPage.css";
+        $title = "Мой путь";
+        $class_reg_btn = "";
+        $class_auth_btn = "";
+        $class_lk_btn = "";
+        $class_lk_btn_none = "nav-el-none";
+        include_once 'header.php';
+    ?>
+
+    <?php
+
+        include_once 'navPanel.php';
+
+    ?>
+
+        <div class="content-panel">
+            <div class="title">
+                 Мой жизненный путь:
+            </div>
+            <div class="row">
+                <div class="row-body">
+                    <div class="text-body">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus commodi soluta officia quos iusto doloremque, corrupti repudiandae similique placeat quis nemo ratione reiciendis fugiat vero beatae ab cumque ex iure quaerat aut. Deleniti et provident a quod. Labore necessitatibus hic consectetur exercitationem, quaerat tempore, repellendus unde repudiandae laboriosam velit laudantium odit officia iste ipsa ullam sequi vero expedita numquam a? Odit nemo distinctio eaque? Amet atque modi accusantium accusamus, facere ullam libero architecto quaerat pariatur voluptatum. Quibusdam nobis ducimus voluptas architecto, corporis magni enim ullam distinctio rem earum quam consequuntur maxime iusto, porro debitis voluptatem? Fugit, eos voluptas? Temporibus, odit!
+                    </div>
+                </div>
+            </div>
+        </div>
+
+            </div>
+        </div>
+    </div>
+
+</div>
--- navPanel.php
+++ navPanel.php
@@ -6,7 +6,7 @@
                         <tr><td onclick="window.location.href='/lkPage.php'; return false">Личный кабинет</td></tr>
                         <tr><td onclick="window.location.href='/myFriendsPage.php'; return false">Мои друзья</td></tr>
                         <tr><td onclick="window.location.href='/myHistoriesPage.php'; return false">Истории из жизни</td></tr>
-                        <tr><td onclick="window.location.href='/lkPage.php'; return false">Мой путь</td></tr>
+                        <tr><td onclick="window.location.href='/myWayPage.php'; return false">Мой путь</td></tr>
                         <tr><td onclick="window.location.href='/lkPage.php'; return false">Любимые фото</td></tr>
                     </table>
                 </div>
