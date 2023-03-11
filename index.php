05a82dc5aa4322d79b076a5b4c8a920ddbc36823
Ivan Borkov <i.borkoff@yandex.ru>
2023-03-11 12:22:12 +0300

    index reg auth pages are adaptives

--- index.php
+++ index.php
@@ -9,13 +9,15 @@
 
     <div class="content">
         <div class="container">
-        <h1>Присоединяйся к нам!</h1>
+            <div class="title-b">
+                Присоединяйся к нам!
+            </div>
             <div class="row">
                 <div class="pic">
                     <img src="images/image_1_u11.jpg" alt="">
                 </div>
                 <div class="text">
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit animi impedit laboriosam iusto doloribus voluptas, nemo at sed quo eaque nobis rem officia adipisci, temporibus quas perspiciatis obcaecati. Libero cupiditate ipsam et dolorum, unde dicta minus officia dignissimos minima soluta nihil, sit repudiandae incidunt molestias cumque reiciendis vitae itaque vero impedit nam porro. Facilis minima quaerat dolor consequatur consequuntur ab suscipit deleniti dignissimos recusandae quas. In deserunt quam ex ea beatae molestiae ad cumque et adipisci repudiandae molestias saepe excepturi fuga necessitatibus officia aperiam dolores doloribus, minima cum qui iusto? Repellendus distinctio fugit, at ex vero placeat sit ea magnam.</p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit animi impedit laboriosam iusto doloribus voluptas, nemo at sed quo eaque nobis rem officia adipisci, temporibus quas perspiciatis obcaecati. Libero cupiditate ipsam et dolorum, unde dicta minus officia dignissimos minima soluta nihil, sit repudiandae incidunt molestias cumque reiciendis vitae itaque vero impedit nam porro. Facilis minima quaerat dolor consequatur consequuntur ab suscipit deleniti dignissimos recusandae quas. In deserunt quam ex ea beatae molestiae ad cumque et adipisci repudiandae molestias saepe excepturi fuga necessitatibus officia aperiam dolores doloribus, minima cum qui iusto? Repellendus distinctio fugit, at ex vero placeat sit ea magnam.
                 </div>
             </div>
         </div>
